0c521421348cfb40d35edde818a14cde4a7fbf38
Paul Haedrich <paul@berrysauce.me>
2023-03-10 20:50:42 +0100

    Added spacing for new Reload component

--- postoffice/src/lib/Reload.svelte
+++ postoffice/src/lib/Reload.svelte
@@ -1,6 +1,6 @@
 <main>
-    <div style="padding: 20px; margin-bottom: 20px; padding-right: 40px; padding-left: 40px;">
-        <h4 class="text-center" style="font-family: Inter, sans-serif; letter-spacing: -0.5px; margin-top: 15vh;">
+    <div style="padding: 20px; margin-bottom: 20px; padding-right: 40px; padding-left: 40px; margin-top: 5vh; margin-bottom: 5vh;">
+        <h4 class="text-center" style="font-family: Inter, sans-serif; letter-spacing: -0.5px;">
           <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" style="font-size: 40px;color: rgb(90,179,75);margin-bottom: 10px;">
             <path d="M4 4V9H4.58152M19.9381 11C19.446 7.05369 16.0796 4 12 4C8.64262 4 5.76829 6.06817 4.58152 9M4.58152 9H9M20 20V15H19.4185M19.4185 15C18.2317 17.9318 15.3574 20 12 20C7.92038 20 4.55399 16.9463 4.06189 13M19.4185 15H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
           </svg><br>
