5d560a35a83574e641fb79c53acb4b5f9cf95fd1
Paul Haedrich <hello@berrysauce.me>
2024-02-06 11:00:08 +0100

    Changed away banner text to center

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -103,7 +103,7 @@
 <!-- Away Banner -->
 {#if !reloadActive}
     <div style="background: var(--bs-red);padding: 16px;">
-        <p style="margin-bottom: 0px;color: rgba(255,255,255,0.8);font-weight: 500;">
+        <p class="text-center" style="margin-bottom: 0px;color: rgba(255,255,255,0.8);font-weight: 500;">
             <span style="font-weight: 600;color: rgb(255,255,255);">
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" style="font-size: 20px;margin-top: -4px;margin-right: 8px;">
                     <path d="M12 8V12M12 16H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
